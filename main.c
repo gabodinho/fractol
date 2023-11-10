445c403cc3e61cbe1a7d822fb18c4964093df218
gabodinho <g.giertzuch@hotmail.de>
2023-11-10 13:20:45 +0100

    test: 1st image

---
+++ main.c
@@ -0,0 +1,237 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggiertzu <ggiertzu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 11:57:54 by ggiertzu          #+#    #+#             */
+/*   Updated: 2023/11/10 13:18:17 by ggiertzu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <math.h>
+#include <MLX42/MLX42.h>
+#include <string.h>
+
+#define WIDTH 256
+#define HEIGHT 256
+#ifndef THRESH
+# define THRESH 5
+# endif
+#ifndef MAXRUNS
+# define MAXRUNS 20
+# endif
+
+//
+//	----- input from utils ----
+//
+void	comp_sq(double *comp)
+{
+	double	real;
+	double	img;
+
+	if (!comp)
+		return;
+	real = comp[0];
+	img = comp[1];
+	comp[0] = pow(real, 2) - pow(img, 2);
+	comp[1] = real * img;
+}
+
+void	comp_sum(double *comp1, double *comp2)
+{
+	if (!comp1 || !comp2)
+		return;
+	comp1[0] = comp1[0] + comp2[0];
+	comp1[1] = comp1[1] + comp2[1];
+}
+
+double	comp_abs(double *comp)
+{
+	double	res;
+
+	if (!comp)
+		return (-1);
+	res = sqrt(pow(comp[0], 2) + pow(comp[1], 2));
+	return res;
+}
+
+void	printc(double *comp)
+{
+	printf("real: %f img: %f\n", comp[0], comp[1]);
+}
+
+double	mandel_step(double *prev, double *c)
+{
+	if (!prev || !c)
+		return (-1);
+	comp_sq(prev);
+	comp_sum(prev, c);
+	return (comp_abs(prev));
+}
+
+/* idea to take a function as an argument to work with other sets. Prototype looks like this:
+int	steps(void *(*set)(double *, double *),double *point, int max, double thresh)
+*/
+
+int steps_mandel(double *point)
+{
+	int	i;
+	double	res;
+	double	prev[2];
+
+	i = 0;
+	res = -1;
+	prev[0] = 0;
+	prev[1] = 0;
+// check input in the beginning
+	while (i < MAXRUNS && res < THRESH)
+	{
+		res = mandel_step(prev, point);
+//		printf("%f\n", res);
+		i++;
+	}
+//	printf("%d\n", i);
+	return i;
+}
+//
+//	----- end utils -----
+//
+
+typedef	struct limits_s
+{
+	double	x_min;
+	double	x_max;
+	double	y_min;
+	double	y_max;
+	double	delta_x;
+	double	delta_y;
+}	limits_t;
+
+limits_t	init_lim(void)
+{
+	limits_t	lim;
+
+	lim.x_min = -2.5;
+	lim.x_max = 0.5;
+	lim.y_min = -1.5;
+	lim.y_max = 1.5;
+	lim.delta_x = (lim.x_max - lim.x_min) / WIDTH;
+	lim.delta_y = (lim.y_max - lim.y_min) / HEIGHT;
+	return (lim);
+}
+
+int	get_steps(int i, int j, limits_t lim)
+{
+	double	point[2];
+
+	point[0] = lim.x_min + (lim.delta_x * i);
+	point[1] = lim.y_max - (lim.delta_y * j);
+	return (steps_mandel(point));
+}
+
+// black to red
+int	cmap_def(int steps)
+{
+	uint8_t	red;
+	uint8_t	green;
+
+	if (steps == MAXRUNS)
+	{
+		green = 0;
+		red = 0;
+	}
+	else if (MAXRUNS / 2 <= steps)
+	{
+		green = 0;
+		red = -410 * steps / MAXRUNS + 460;
+	}
+	else
+	{
+		red = 255;
+		green = 255 * 2 * (MAXRUNS - steps) / MAXRUNS;
+	}
+	return ((red << 24) | (green << 16) | (0x00 << 8) | 0xFF);
+}
+
+int	get_colour(int steps, int cmap)
+{
+	if (cmap == 1)
+		return (cmap_def(steps));
+	return (-1);
+}
+
+void	put_colour(uint8_t *pixels, int rgba)
+{
+	if (!pixels)
+		return ;
+	*pixels = (rgba >> 24) & 0xFF;
+	*(pixels+1) = (rgba >> 16) & 0xFF;
+	*(pixels+2) = (rgba >> 8) & 0xFF;
+	*(pixels+3) = (rgba) & 0xFF;
+	return ;
+}
+
+void	draw_image(mlx_image_t *img, limits_t lim)
+{
+	int i;
+	int	j;
+	int	pixel_idx;
+	int colour;
+	
+	i = 0;
+	pixel_idx = 0;
+	colour = 0;
+	while (i < WIDTH)
+	{
+		j = 0;
+		while (j < HEIGHT)
+		{
+			pixel_idx = (j * WIDTH * 4) + (i * 4);
+			colour = get_colour(get_steps(i, j, lim), 1);
+			put_colour((img -> pixels) + pixel_idx, colour);
+//			printf("%d %d %d %d\n", img -> pixels[pixel_idx], img->pixels[pixel_idx+1], img->pixels[pixel_idx+2],img->pixels[pixel_idx+3]);
+			j++;
+		}
+		i++;
+	}
+}
+
+int32_t main(int32_t argc, const char* argv[])
+{
+	mlx_t* mlx;
+	mlx_image_t* image;
+	limits_t	lim;
+
+	// Gotta error check this stuff
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+	{
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (!(image = mlx_new_image(mlx, WIDTH, HEIGHT)))
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	lim = init_lim();
+	draw_image(image, lim);
+    mlx_image_to_window(mlx, image, 0, 0);
+//	mlx_loop_hook(mlx, ft_hook, mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
---
+++ test/test3.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test3.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggiertzu <ggiertzu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 15:18:40 by ggiertzu          #+#    #+#             */
+/*   Updated: 2023/11/03 22:11:40 by ggiertzu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// -----------------------------------------------------------------------------
+// Codam Coding College, Amsterdam @ 2022-2023 by W2Wizard.
+// See README in the root project for more information.
+// -----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <MLX42/MLX42.h>
+
+#define WIDTH 256
+#define HEIGHT 256
+
+static mlx_image_t* image;
+
+// -----------------------------------------------------------------------------
+
+int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+{
+    return (r << 24 | g << 16 | b << 8 | a);
+}
+
+void ft_randomize(void* param)
+{
+	uint32_t color = ft_pixel(
+                 rand() % 0xFF, // R
+                 rand() % 0xFF, // G
+                 rand() % 0xFF, // B
+                 rand() % 0xFF  // A
+			);
+	for (int32_t i = 0; i < image->width; ++i)
+	{
+		for (int32_t y = 0; y < image->height; ++y)
+		{
+			mlx_put_pixel(image, i, y, color);
+		}
+	}
+}
+
+void ft_put(void* param)
+{
+	uint32_t color = ft_pixel(
+                 0x00, // R
+                 0x00, // G
+                 0x00, // B
+                 0xFF  // A
+			);
+	for (int32_t i = 0; i < image->width; ++i)
+	{
+		for (int32_t y = 0; y < image->height; ++y)
+		{
+			color = ft_pixel(i, y, 0, 0xFF);
+			mlx_put_pixel(image, i, y, color);
+		}
+	}
+}
+
+void ft_hook(void* param)
+{
+	mlx_t* mlx = param;
+
+	if (mlx_is_key_down(mlx, MLX_KEY_C))
+		ft_randomize(param);
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		image->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		image->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		image->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		image->instances[0].x += 5;
+}
+
+// -----------------------------------------------------------------------------
+
+int32_t main(int32_t argc, const char* argv[])
+{
+	mlx_t* mlx;
+
+	// Gotta error check this stuff
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+	{
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (!(image = mlx_new_image(mlx, WIDTH, HEIGHT)))
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	ft_put((void *) NULL);
+//	mlx_loop_hook(mlx, ft_randomize, mlx);
+	mlx_loop_hook(mlx, ft_hook, mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
+
